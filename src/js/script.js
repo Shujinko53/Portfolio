79bf2330eb8190f518625e3d01d3752dd65501ad
Shujinko53 <shujinko53@yandex.ru>
2021-03-24 20:48:41 +0300

    The script for a first screen animation has been added

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeBtn = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
